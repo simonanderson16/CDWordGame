69271d60f2a2d08fca76e7a3c84c428fe43920fa
simonanderson16 <uhx8bu@virginia.edu>
2024-05-30 14:09:48 -0400

    initial file setup

---
+++ make-game/src/pages/Make.tsx
@@ -0,0 +1,9 @@
+const Make = () => {
+  return (
+    <div>
+      <h1>Make Game</h1>
+    </div>
+  );
+};
+
+export default Make;
---
+++ play-game/src/pages/Play.tsx
@@ -0,0 +1,9 @@
+const Play = () => {
+  return (
+    <div>
+      <h1>Play Game</h1>
+    </div>
+  );
+};
+
+export default Play;
