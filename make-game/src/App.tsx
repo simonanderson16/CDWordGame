c1a7c5e154585e022a0f0583668e09aca74df352
Simon Anderson <uhx8bu@virginia.edu>
2024-06-02 18:34:57 -0400

    set tab title and icon

--- make-game/src/App.tsx
+++ make-game/src/App.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <div>
       <h1>Welcome!</h1>
-      <Link to="/create">Create a Game</Link>
-      <Link to="/games">All Games</Link>
+      <Link to="/create">
+        <Button>Create a Game</Button>
+      </Link>
+      <Link to="/games">
+        <Button>All Games</Button>
+      </Link>
     </div>
   );
 }
