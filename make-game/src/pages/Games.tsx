bf26bd08829970c7af8386d96084c7c17e32c6ea
simonanderson16 <uhx8bu@virginia.edu>
2024-07-09 20:00:03 -0400

    ability to delete current game

--- make-game/src/pages/Games.tsx
+++ make-game/src/pages/Games.tsx
@@ -1,12 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
 import { HomeIcon } from "@radix-ui/react-icons";
 import "../styles/Games.css";
 import { onAuthStateChanged } from "firebase/auth";
@@ -19,103 +14,93 @@ import { Game } from "../types.ts";
 import FutureGames from "./FutureGames.tsx";
 
 const Games = () => {
-  const navigate = useNavigate();
-  const [tab, setTab] = useState<string>("today");
-  const [allGames, setAllGames] = useState<Game[]>([]);
-  const [totalPlays, setTotalPlays] = useState<number>(0);
+    const navigate = useNavigate();
+    const [tab, setTab] = useState<string>("today");
+    const [allGames, setAllGames] = useState<Game[]>([]);
+    const [totalPlays, setTotalPlays] = useState<number>(0);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (!user) {
-        navigate("/");
-      }
-    });
-    return () => unsubscribe();
-  }, [navigate]);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                navigate("/");
+            }
+        });
+        return () => unsubscribe();
+    }, [navigate]);
 
-  const onTabChange = (value: string) => {
-    setTab(value);
-  };
+    const onTabChange = (value: string) => {
+        setTab(value);
+    };
 
-  const findTotalPlays = (games: Game[]) => {
-    let sum = 0;
-    games.forEach((game) => (sum += game.plays));
-    setTotalPlays(sum);
-  };
+    const findTotalPlays = (games: Game[]) => {
+        let sum = 0;
+        games.forEach((game) => (sum += game.plays));
+        setTotalPlays(sum);
+    };
 
-  const getAllGames = async () => {
-    const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/create`);
-    setAllGames(response.data);
-    findTotalPlays(response.data);
-  };
+    const getAllGames = async () => {
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/create`);
+        setAllGames(response.data);
+        findTotalPlays(response.data);
+    };
 
-  useEffect(() => {
-    getAllGames();
-  }, []);
+    useEffect(() => {
+        getAllGames();
+    }, []);
 
-  return (
-    <div className="games-container">
-      <h1 className="games-header">Hoos Spelling: All Games</h1>
-      <h2 className="games-subheader white-box mb-3">{totalPlays} All-Time Plays</h2>
-      <Link to="/">
-        <Button className="home-button">
-          <HomeIcon className="mr-2 h-4 w-4" />
-          Home
-        </Button>
-      </Link>
-      <Tabs
-        defaultValue="today"
-        className="tabs mb-5"
-        value={tab}
-        onValueChange={onTabChange}
-      >
-        <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="past">Past Games</TabsTrigger>
-          <TabsTrigger value="today">Today's Game</TabsTrigger>
-          <TabsTrigger value="future">Future Games</TabsTrigger>
-        </TabsList>
-        <TabsContent value="past">
-          <Card className="games-card scrollable-card">
-            <CardHeader>
-              <CardDescription>
-                All of the following games have already ended. You can delete
-                each one you would no longer like to store.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <PastGames allGames={allGames} getAllGames={getAllGames} />
-            </CardContent>
-          </Card>
-        </TabsContent>
-        <TabsContent value="today">
-          <Card className="games-card">
-            <CardHeader>
-              <CardDescription>
-                The following game is currently active. You will not be able to
-                delete it until it is no longer live.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <TodaysGame allGames={allGames} />
-            </CardContent>
-          </Card>
-        </TabsContent>
-        <TabsContent value="future">
-          <Card className="games-card scrollable-card">
-            <CardHeader>
-              <CardDescription>
-                All of the following games have been created but not yet
-                released. You can delete any of these games.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <FutureGames allGames={allGames} getAllGames={getAllGames} />
-            </CardContent>
-          </Card>
-        </TabsContent>
-      </Tabs>
-    </div>
-  );
+    return (
+        <div className="games-container">
+            <h1 className="games-header">Hoos Spelling: All Games</h1>
+            <h2 className="games-subheader white-box mb-3">{totalPlays} All-Time Plays</h2>
+            <Link to="/">
+                <Button className="home-button">
+                    <HomeIcon className="mr-2 h-4 w-4" />
+                    Home
+                </Button>
+            </Link>
+            <Tabs defaultValue="today" className="tabs mb-5" value={tab} onValueChange={onTabChange}>
+                <TabsList className="grid w-full grid-cols-3">
+                    <TabsTrigger value="past">Past Games</TabsTrigger>
+                    <TabsTrigger value="today">Current Game</TabsTrigger>
+                    <TabsTrigger value="future">Future Games</TabsTrigger>
+                </TabsList>
+                <TabsContent value="past">
+                    <Card className="games-card scrollable-card">
+                        <CardHeader>
+                            <CardDescription>
+                                All of the following games have already ended. You can delete each one you would no longer like to store.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <PastGames allGames={allGames} getAllGames={getAllGames} />
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="today">
+                    <Card className="games-card">
+                        <CardHeader>
+                            <CardDescription>The following game is currently active.</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <TodaysGame allGames={allGames} getAllGames={getAllGames}/>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="future">
+                    <Card className="games-card scrollable-card">
+                        <CardHeader>
+                            <CardDescription>
+                                All of the following games have been created but not yet released. You can delete any of these games.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <FutureGames allGames={allGames} getAllGames={getAllGames} />
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+        </div>
+    );
 };
 
 export default Games;
--- make-game/src/pages/TodaysGame.tsx
+++ make-game/src/pages/TodaysGame.tsx
@@ -1,63 +1,94 @@
 import { useEffect, useState } from "react";
 import { Game } from "../types.ts";
+import { Button } from "@/components/ui/button.tsx";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import axios from "axios";
 
-const TodaysGame = ({ allGames }: { allGames: Game[] }) => {
-  const [todaysGame, setTodaysGame] = useState<Game>();
+const TodaysGame = ({ allGames, getAllGames }: { allGames: Game[]; getAllGames: () => void }) => {
+    const [todaysGame, setTodaysGame] = useState<Game>();
 
-  useEffect(() => {
-    const today = new Date();
-    const gameForToday = allGames.find((game) => {
-      const fromDate = new Date(game.dates.from);
-      const toDate = new Date(game.dates.to);
-      return fromDate <= today && toDate >= today;
-    });
-    setTodaysGame(gameForToday);
-  }, [allGames]);
+    useEffect(() => {
+        const today = new Date();
+        const gameForToday = allGames.find((game) => {
+            const fromDate = new Date(game.dates.from);
+            const toDate = new Date(game.dates.to);
+            return fromDate <= today && toDate >= today;
+        });
+        setTodaysGame(gameForToday);
+    }, [allGames]);
 
-  function formatDateString(dateString: string) {
-    const date = new Date(dateString);
-    return date.toDateString();
-  }
+    const formatDateString = (dateString: string) => {
+        const date = new Date(dateString);
+        return date.toDateString();
+    };
 
-  return (
-    <div>
-      {todaysGame ? (
+    const deleteGame = async (id: string) => {
+        await axios.delete(`${import.meta.env.VITE_SERVER_URL}/create/${id}`);
+        getAllGames();
+    };
+
+    return (
         <div>
-          <h2 className="white-box">{todaysGame.plays} Total Plays</h2>
-          <div className="today-grid">
-            <div className="grid-item-1 grid-item">
-              <p className="font-bold">Available Until</p>
-              <p className="white-box">
-                {formatDateString(todaysGame.dates.to)}
-              </p>
-            </div>
-            <div className="grid-item-2 grid-item">
-              <p className="font-bold">Required Letter</p>
-              <p className="white-box">{todaysGame.requiredLetter}</p>
-            </div>
-            <div className="grid-item-3 grid-item">
-              <p className="font-bold">Additional Letters</p>
-              <p className="white-box">
-                {todaysGame.availableLetters.join(" ")}
-              </p>
-            </div>
-            <div className="grid-item-4 grid-item">
-              <p className="font-bold">Answers</p>
-              <div className="todays-answers">
-                {todaysGame.answers.map((answer: string, index: number) => (
-                  <p key={index} className="white-box">
-                    {answer}
-                  </p>
-                ))}
-              </div>
-            </div>
-          </div>
+            {todaysGame ? (
+                <div>
+                    <div className="flex justify-between">
+                        <h2 className="white-box">{todaysGame.plays} Total Plays</h2>
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button variant="destructive">
+                                    <TrashIcon className="mr-2 h-4 w-4" /> Delete
+                                </Button>
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Delete Game?</DialogTitle>
+                                    <DialogDescription>
+                                        This action cannot be undone. This will permanently delete the current game and people will no longer be able to play
+                                        it. It is advised that you reserve this action for games that contain errors.
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <Button
+                                    variant="destructive"
+                                    onClick={() => {
+                                        deleteGame(todaysGame.id);
+                                    }}
+                                >
+                                    <TrashIcon className="mr-2 h-4 w-4" /> Confirm Delete
+                                </Button>
+                            </DialogContent>
+                        </Dialog>
+                    </div>
+                    <div className="today-grid">
+                        <div className="grid-item-1 grid-item">
+                            <p className="font-bold">Available Until</p>
+                            <p className="white-box">{formatDateString(todaysGame.dates.to)}</p>
+                        </div>
+                        <div className="grid-item-2 grid-item">
+                            <p className="font-bold">Required Letter</p>
+                            <p className="white-box">{todaysGame.requiredLetter}</p>
+                        </div>
+                        <div className="grid-item-3 grid-item">
+                            <p className="font-bold">Additional Letters</p>
+                            <p className="white-box">{todaysGame.availableLetters.join(" ")}</p>
+                        </div>
+                        <div className="grid-item-4 grid-item">
+                            <p className="font-bold">Answers</p>
+                            <div className="todays-answers">
+                                {todaysGame.answers.map((answer: string, index: number) => (
+                                    <p key={index} className="white-box">
+                                        {answer}
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <h2>No game today</h2>
+            )}
         </div>
-      ) : (
-        <h2>No game today</h2>
-      )}
-    </div>
-  );
+    );
 };
 
 export default TodaysGame;
